3051f3b964b95d232ec2248f6ec8cc2c36d73bdc
goyalankit <ankitunique@yahoo.com>
2013-03-31 17:52:15 +0530

    adding db url

--- php/connection_open.php
+++ php/connection_open.php
@@ -1,12 +1,11 @@
 <?php
 //insert
 //insert
-  $hostname=$OPENSHIFT_MYSQL_DB_HOST;
-  $port=$OPENSHIFT_MYSQL_DB_PORT;
+  define('DB_HOST', $_ENV['OPENSHIFT_MYSQL_DB_HOST'] . ':' . $_ENV['OPENSHIFT_MYSQL_DB_PORT']);
   $username='adminX8efWX5';
   $password='v4Z1h8Kcp5QI';
   $database='alumweb';
-  $connString = "mysql:host=$hostname;dbname=website";
+  $connString = "mysql:host=$DB_HOST;dbname=alumweb";
 try{
       
     $dbh = new PDO($connString,$username,$password);
