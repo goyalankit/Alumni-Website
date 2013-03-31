9f8c5db2c388329a70ce4f19ca1e5f8e79011c76
goyalankit <ankitunique@yahoo.com>
2013-03-31 17:56:25 +0530

    adding db url

--- php/connection_open.php
+++ php/connection_open.php
@@ -6,7 +6,7 @@
   $username='adminX8efWX5';
   $password='v4Z1h8Kcp5QI';
   $database='alumweb';
-  $connString = "mysql:host=$hostname:$port;dbname=alumweb";
+  $connString = "mysql:host=$hostname;port=$port;dbname=alumweb";
 try{
       
     $dbh = new PDO($connString,$username,$password);
